8ea52890de230e76f4710fb718877b5ebe0c0b69
lenovo <343910768@qq.com>
2019-07-03 15:26:55 +0800

    pom

--- src/main/java/com/bjtuxxq/demo/log/LogAspect.java
+++ src/main/java/com/bjtuxxq/demo/log/LogAspect.java
@@ -1,4 +1,14 @@
 package com.bjtuxxq.demo.log;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LogAspect {
+    @Pointcut("@annotation(test.Log)")
+    public void loPointCut(){
+
+    }
 }
