42fa5af270587a733ad865feef6eeabf245ecb69
pyx <16301014@bjtu.edu.cn>
2019-07-05 22:58:48 +0800

    logger support array param request

--- src/main/java/edu/bjtu/xxq/controller/TestController.java
+++ src/main/java/edu/bjtu/xxq/controller/TestController.java
@@ -1,13 +1,18 @@
 package edu.bjtu.xxq.controller;
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final Gson gson = new Gson();
+
     @RequestMapping("/test")
-    public String test(){
+    public String test(@RequestParam("a[]") int[] a) {
+        System.out.println(gson.toJson(a));
         return "test";
     }
 
