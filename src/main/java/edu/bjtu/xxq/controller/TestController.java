d9740b199c58d3d7570b8c91aa3edc00a405efbd
lenovo <343910768@qq.com>
2019-07-06 23:37:32 +0800

    test

--- src/main/java/edu/bjtu/xxq/controller/TestController.java
+++ src/main/java/edu/bjtu/xxq/controller/TestController.java
@@ -1,11 +1,18 @@
 package edu.bjtu.xxq.controller;
 
 import com.google.gson.Gson;
+import edu.bjtu.xxq.model.Book;
+import edu.bjtu.xxq.model.Order;
 import edu.bjtu.xxq.service.BookService;
 import edu.bjtu.xxq.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -25,7 +32,13 @@ public class TestController {
 
     @RequestMapping("/order")
     public String testOrder() {
-        return gson.toJson(orderService.getOne(150001));
+        byte[] k ={1,1,1};
+        Order order = new Order().setCustomer(1).setPrice("1").setState(true).setTime("1111-1-1");
+        Map<Integer,Integer> books = new HashMap<Integer, Integer>();
+        books.put(110001,1);
+        books.put(110002,2);
+        Book book = new Book().setName("asdqwe").setDescription("asdqwe").setAuthor("asdqwe").setPublisher("asdqwe").setPublished("1111-1-1").setISBN("11111").setPrice("11.12");
+        return gson.toJson(orderService.addOrder(order,books));
     }
 
 }
