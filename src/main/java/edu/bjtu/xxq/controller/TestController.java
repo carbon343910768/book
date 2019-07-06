89917a77aaa1530226457c730d9d11f7b2a3d102
pyx <16301014@bjtu.edu.cn>
2019-07-06 13:30:30 +0800

    TestController

--- src/main/java/edu/bjtu/xxq/controller/TestController.java
+++ src/main/java/edu/bjtu/xxq/controller/TestController.java
@@ -5,21 +5,26 @@ import edu.bjtu.xxq.service.BookService;
 import edu.bjtu.xxq.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     private final Gson gson = new Gson();
     @Autowired
     private BookService bookService;
 
+    @RequestMapping("")
+    public String test() {
+        return "test";
+    }
+
     @Autowired
     private OrderService orderService;
 
-    @RequestMapping("/test")
-    public String test() {
+    @RequestMapping("/order")
+    public String testOrder() {
         return gson.toJson(orderService.getOne(150001));
     }
 
