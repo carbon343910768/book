0ee70f9c7148274baf9a5b5574929f9aee97958f
lenovo <343910768@qq.com>
2019-07-06 11:55:44 +0800

    bookDao loadBookByTag

--- src/main/java/edu/bjtu/xxq/service/BookService.java
+++ src/main/java/edu/bjtu/xxq/service/BookService.java
@@ -13,12 +13,16 @@ public class BookService {
     @Autowired
     private BookDao bookDao;
 
-//    public List<Book> loadBookByTag(String tag) {
-//        return bookDao.findBookByTag(tag);
-//    }
+    public List<Book> loadBooksByTag(String tag, int start,int limit) {
+        return bookDao.findBooksByTag(tag,start,limit);
+    }
 
     public Book getOne(int id) {
         return bookDao.findBookById(id);
+    }
+
+    public List<Book> loadBooksById(List<Integer> id){
+        return bookDao.findBooksById(id);
     }
 
 //    public List<Book> loadBookByName(String bookName) {
