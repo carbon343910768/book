fd83aacf36248f50d5cb2c3c2b7e3dfc4a9c1a5e
lenovo <343910768@qq.com>
2019-07-08 10:06:29 +0800

    recommend

---
+++ src/main/java/edu/bjtu/xxq/service/RecommendService.java
@@ -0,0 +1,22 @@
+package edu.bjtu.xxq.service;
+
+
+import edu.bjtu.xxq.dao.RecommendDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecommendService {
+    @Autowired
+    private RecommendDao recommendDao;
+
+    List<Integer> recommendByUser(int id, int count){
+        return recommendDao.recommendByUser(id,count);
+    }
+
+    List<Integer> recommendByBook(int id, int count){
+        return recommendDao.recommendByBook(id,count);
+    }
+}
