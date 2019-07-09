9c69311f8e36dcadabafce4788985cee94b6193f
pyx <16301014@bjtu.edu.cn>
2019-07-09 10:29:40 +0800

    logger use thread pool

--- src/main/java/edu/bjtu/xxq/service/LoggerService.java
+++ src/main/java/edu/bjtu/xxq/service/LoggerService.java
@@ -4,17 +4,23 @@ import edu.bjtu.xxq.dao.LoggerDao;
 import edu.bjtu.xxq.model.RequestLog;
 import edu.bjtu.xxq.util.UserUtil;
 import edu.bjtu.xxq.util.WeightUtil;
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoggerService {
 
     @Autowired
     private LoggerDao loggerDao;
+    private Executor executor = new ThreadPoolExecutor(4, 4, 1, TimeUnit.HOURS, new LinkedBlockingDeque<>());
 
     public void saveLog(RequestLog log) {
-        loggerDao.save(log);
+        executor.execute(() -> loggerDao.save(log));
     }
 
     public void action(Integer bookId, int action) {
@@ -25,6 +31,6 @@ public class LoggerService {
         Integer weight = WeightUtil.weight(action);
         Integer userId = UserUtil.getUserId();
         if (weight != null && userId != null)
-            loggerDao.record(userId, bookId, action, weight);
+            executor.execute(() -> loggerDao.record(userId, bookId, action, weight));
     }
 }
